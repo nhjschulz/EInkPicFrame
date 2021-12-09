0dcc5167358dad6ee529050a18022da88d93bc51
Haju Schulz <haju.schulz@online.de>
2021-12-09 12:01:11 +0100

    AVR SW environment setup
    * Atmel ICE programming in ISP mode working
    * "Blinky" app working on breadboard (LED on PB0)

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <avr/io.h>
-
+#include <avr/delay.h>
 int main(int argc, char** argv)
 {
-        
+    
+    DDRB |= (1<<PB0);
+    PORTB &= ~(1<<PB0);
+    PORTB |= (1<<PB0);
     for(;;)
     {
+        PORTB &= ~(1<<PB0);
+        _delay_ms(500);
+           
+        PORTB |= (1<<PB0);
+        _delay_ms(500);
 
     }
 
